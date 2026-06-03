af10165fbde9c1706fe76951fb1e1a844f650e3a
VisionHire <sarojniklesh1@gmail.com>
2026-06-03 16:25:46 +0530

    Fix abort import in blog routes

--- full web/app.py
+++ full web/app.py
@@ -1337,9 +1337,8 @@ def sitemap():
 def nutrition_info():
     return render_template('nutrition_system_info.html')
 
+from flask import render_template, abort
 from articles import get_all_articles, get_article_by_slug
-
-# Your existing routes (home, about, etc.) stay untouched.
 
 @app.route('/blog')
 def blog_index():
@@ -1351,7 +1350,6 @@ def blog_post(slug):
     article = get_article_by_slug(slug)
     if not article:
         abort(404)
-    # The template name matches the slug: calories-in-roti.html
     return render_template(f'blog/{slug}.html', article=article)
 
 # --------------------------------------------------
